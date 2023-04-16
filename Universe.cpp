124001d450ad36c05283c75e4ef8d816583bfb70
Eric <eric.dianhao.wu@gmail.com>
2023-04-15 19:36:23 -0700

    move some files, remove some dead code
    much more changes to come.

--- Universe.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Universe.cpp
-//  Neuroevolution_project1
-//
-//  Created by Liyu Zerihun on 1/25/23.
-//  Copyright © 2023 Liyu Zerihun. All rights reserved.
-//
-
--- organism.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  organism.cpp
-//  Neuroevolution_project1
-//
-//  Created by Liyu Zerihun on 2/9/23.
-//  Copyright © 2023 Liyu Zerihun. All rights reserved.
-//
-
